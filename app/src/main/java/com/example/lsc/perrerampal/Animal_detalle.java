1f134e4ab51733d7b419d89d4bef7d449b578ad0
kzrrz <siskopls@grr.la>
2017-05-15 12:50:59 -0500

    Versión 0.8: -Se añadió pantalla "About" con detalles de los desarrolladores y el proyecto así como un enlace al GitHub. -Se añadió funcionalidad de llamar al 072. -Se añadieron botones de inscribirse al taller y adoptar perro. -Se añadieron funcionalidades de ubicación del dispositivo usuario. -Se incorporó funconalidad de recibir notificaciones a través de Firebase. -Se incorporaron formularios de solicitud de adopción e inscripción a taller. :v

--- app/src/main/java/com/example/lsc/perrerampal/Animal_detalle.java
+++ app/src/main/java/com/example/lsc/perrerampal/Animal_detalle.java
@@ -1,11 +1,14 @@
 package com.example.lsc.perrerampal;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Animal_detalle extends AppCompatActivity {
 
@@ -27,11 +30,51 @@ public class Animal_detalle extends AppCompatActivity {
         nombre.setText("Nombre: " + getIntent().getStringExtra("nombre"));
         descripcion.setText("Descripción: " + getIntent().getStringExtra("descripcion"));
 
+        inscripBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                solicitudAdopcion(view);
+            }
+        });
+
     }
 
-    public void inscribirTaller(View v)
+
+    public void solicitudAdopcion(View v)
     {
-        //Código del onClick para inscribirse al taller.
+        //Código para la inscripción al taller. Añadir botón enviar y cancelar y hacer que muestre todos los campos
+        AlertDialog.Builder alerta = new AlertDialog.Builder(Animal_detalle.this);
+        View aView = getLayoutInflater().inflate(R.layout.solicitud_adopcion_dialog, null);
+
+        //Inicializamos los elementos mediante aView porque éstos no están en Taller_detalle sino en otro lugar
+        // y a través del inflater llegamos a él.
+        final EditText etNombre = (EditText) aView.findViewById(R.id.nombreAdopcionET);
+        final EditText etApell = (EditText) aView.findViewById(R.id.apellidosAdopcionET);
+        final EditText etDir = (EditText) aView.findViewById(R.id.dirSolAdopcionET);
+        final EditText etTel = (EditText) aView.findViewById(R.id.telSolAdopcionET);
+        final EditText etMail = (EditText) aView.findViewById(R.id.emailSolAdopET);
+        Button inscripBtn = (Button) aView.findViewById(R.id.adoptarBtn);
+
+        //Evento del botón "Inscribir Taller"
+        inscripBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Si los campos no están vacíos
+                if (!etNombre.getText().toString().isEmpty() && !etApell.getText().toString().isEmpty() &&
+                        etDir.getText().toString().isEmpty() && etTel.getText().toString().isEmpty() &&
+                        etMail.getText().toString().isEmpty()) {
+                    Toast.makeText(Animal_detalle.this, "Enviando solicitud de adopción", Toast.LENGTH_SHORT).show();
+                    //Enviar solicitud PHP de inscripción
+                }
+                //Si los campos están vacíos mostrar este Toast
+                else
+                    Toast.makeText(Animal_detalle.this, "Llene todos los campos, por favor", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Finalmente construímos todo lo anterior y lo mostramos.
+        alerta.setView(aView);
+        alerta.create().show();
     }
 
 }
--- app/src/main/java/com/example/lsc/perrerampal/Taller_detalle.java
+++ app/src/main/java/com/example/lsc/perrerampal/Taller_detalle.java
@@ -1,17 +1,25 @@
 package com.example.lsc.perrerampal;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Taller_detalle extends AppCompatActivity {
 
     ImageView imagen;
     TextView nombre, descripcion;
     Button accionBtn;
+    RelativeLayout Layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +34,50 @@ public class Taller_detalle extends AppCompatActivity {
         imagen.setImageResource(getIntent().getIntExtra("imagen",00));
         nombre.setText("Nombre: " + getIntent().getStringExtra("nombre"));
         descripcion.setText("Descripción: " + getIntent().getStringExtra("descripcion"));
+
+        accionBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                inscribirTaller(view);
+            }
+        });
+
     }
 
-    protected void inscribirTaller(View V)
+    public void inscribirTaller(View v)
     {
-        //Código para la inscripción al taller.
+        //Código para la inscripción al taller. Añadir botón enviar y cancelar y hacer que muestre todos los campos
+        AlertDialog.Builder alerta = new AlertDialog.Builder(Taller_detalle.this);
+        View aView = getLayoutInflater().inflate(R.layout.inscripcion_taller_dialog, null);
+
+        //Inicializamos los elementos mediante aView porque éstos no están en Taller_detalle sino en otro lugar
+        // y a través del inflater llegamos a él.
+        final EditText etNombre = (EditText) aView.findViewById(R.id.nombreSolTallEdit);
+        final EditText etApell = (EditText) aView.findViewById(R.id.apellidosSolTallEdit);
+        final EditText etDir = (EditText) aView.findViewById(R.id.dirSolTallEdit);
+        final EditText etTel = (EditText) aView.findViewById(R.id.telSolTallEdit);
+        final EditText etMail = (EditText) aView.findViewById(R.id.emailSolTallEdit);
+        Button inscripBtn = (Button) aView.findViewById(R.id.inscripTallerBtn);
+
+        //Evento del botón "Inscribir Taller"
+        inscripBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Si los campos no están vacíos
+                if (!etNombre.getText().toString().isEmpty() && !etApell.getText().toString().isEmpty() &&
+                        etDir.getText().toString().isEmpty() && etTel.getText().toString().isEmpty() &&
+                        etMail.getText().toString().isEmpty()) {
+                    Toast.makeText(Taller_detalle.this, "Enviando Inscripción", Toast.LENGTH_SHORT).show();
+                    //Enviar solicitud PHP de inscripción
+                }
+                //Si los campos están vacíos mostrar este Toast
+                else
+                    Toast.makeText(Taller_detalle.this, "Llene todos los campos, por favor", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Finalmente construímos todo lo anterior y lo mostramos.
+        alerta.setView(aView);
+        alerta.create().show();
     }
 }
